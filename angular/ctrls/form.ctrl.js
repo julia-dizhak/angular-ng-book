85422d7af88ef8c9487c2a69feafc3abf18e9747
Yulia <yulia.scherbina@gmail.com>
2016-11-28 18:23:18 +0100

    ng-form, ctrl

---
+++ angular/ctrls/form.ctrl.js
@@ -0,0 +1,10 @@
+app.controller('FormController', function($scope) {
+    $scope.fields = [
+        {placeholder: 'Username', isRequired: true},
+        {placeholder: 'Password', isRequired: true},
+        {placeholder: 'Email (optional)', isRequired: false}
+    ];
+    $scope.submitForm = function() {
+        alert("it works!");
+    };
+});
